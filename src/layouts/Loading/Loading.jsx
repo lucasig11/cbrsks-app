a55860565f05e9ecbe8be87c2f7e994f05d95ae2
Marco Bruno <marco.bruno@meliuz.com.br>
2023-02-10 01:05:15 +0100

    :sparkles: Add space on percentage

--- src/layouts/Loading/Loading.jsx
+++ src/layouts/Loading/Loading.jsx
@@ -4,7 +4,7 @@ const Loading = ({isLoaded, loadingProgression}) => (
   <S.Loading isLoaded={isLoaded}>
     <S.Content>
       <S.Text>Loading_</S.Text>
-      <S.Text>{(loadingProgression * 100).toFixed(2)}%</S.Text>
+      <S.Text>{(loadingProgression * 100).toFixed(2)} %</S.Text>
     </S.Content>
     <S.Bar loadingProgression={loadingProgression} />
   </S.Loading>
