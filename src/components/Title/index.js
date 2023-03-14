487b15175e19864ba69d395bfd844bdda0a120d4
lucas <lucasfonsecard@icloud.com>
2023-03-14 20:35:46 -0300

    fix: remove line break from Title

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,12 +1,16 @@
 import styled, { css } from "styled-components";
 
-const hasOrange = ({ orange }) => orange && css`
-  color: var(--color-second);
-`;
+const hasOrange = ({ orange }) =>
+  orange &&
+  css`
+    color: var(--color-second);
+  `;
 
-const hasBigger = ({ bigger }) => bigger && css`
-  font-size: 9rem;
-`;
+const hasBigger = ({ bigger }) =>
+  bigger &&
+  css`
+    font-size: 9rem;
+  `;
 
 const Title = styled.h1`
   font-size: 3.5rem;
@@ -14,6 +18,7 @@ const Title = styled.h1`
   text-transform: uppercase;
   text-align: center;
   font-weight: bold;
+  white-space: nowrap;
 
   ${hasOrange};
   ${hasBigger};
