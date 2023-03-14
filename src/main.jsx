dd2c49f58f5c50051484bc0b4c2c5699e5fca121
lucas <lucasfonsecard@icloud.com>
2023-03-14 19:05:20 -0300

    fix: use import instead of require

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Buffer } from "buffer";
 
-window.Buffer = window.Buffer || require("buffer").Buffer;
+window.Buffer = window.Buffer || Buffer;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
