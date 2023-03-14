35f370a1390ad39d6a289de4540d2989ec639aa0
lucas <lucasfonsecard@icloud.com>
2023-03-14 19:00:13 -0300

    fix: add node polyfills

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import eslint from "vite-plugin-eslint";
+import nodePolyfills from "vite-plugin-node-stdlib-browser";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    nodePolyfills(),
     eslint({
       cache: true,
       fix: true,
