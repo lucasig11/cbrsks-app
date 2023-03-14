29f861b6741ca8fababc4cff633fee1ce828dd6a
lucas <lucasfonsecard@icloud.com>
2023-03-14 18:46:41 -0300

    fix: add window.Buffer

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+window.Buffer = window.Buffer || require("buffer").Buffer;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
